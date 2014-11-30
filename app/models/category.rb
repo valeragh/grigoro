e42ce25944bb253e67391d9359e752096fc08e08
Eugene Nakonechny <eugene.src@gmail.com>
2014-11-30 16:44:34 +0200

    update has_many through assosiation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :template
-  has_many :item_category_values
+
+  has_many :items, through: :item_category_values
+  has_many :item_category_values, dependent: :destroy
 end
